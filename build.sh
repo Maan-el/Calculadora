900364e2987323d12aebe0ccca617b0550e16f72
Manoel <mancordosa@gmail.com>
2023-04-02 20:55:07 -0300

    Optimizações genericas para x86

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-compiler_linux="gcc"
+compiler_linux64="gcc"
 compiler_windows64="x86_64-w64-mingw32-gcc"
 compiler_windows32="i686-w64-mingw32-gcc"
 
 input="src/main.c"
 
-output="-o build/calculadora"
+# V3 só foi habilitada "universalmente" recentemente, então vou usar só o V2
+x86_optimization="-march=x86-64-v2"
 
 flags=(
     "-Wall"
@@ -37,8 +38,6 @@ flags=(
     "-Wno-unused-parameter"
 )
 
-compilacao() {
-    "$compiler_linux" "${flags[@]}" "$output" "$input"
-    "$compiler_windows64" "${flags[@]}" -o build/calculadora_win64 "$input"
-    "$compiler_windows32" "${flags[@]}" -o build/calculadora_win32 "$input"
-}
+"$compiler_linux64" "$x86_optimization" "${flags[@]}" -o build/calculadora_linux64 "$input"
+"$compiler_windows64" "$x86_optimization" "${flags[@]}" -o build/calculadora_win64 "$input"
+"$compiler_windows32" "${flags[@]}" -o build/calculadora_win32 "$input"
