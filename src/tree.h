420cdd9d87c4e53c6759f63d8adfde88df533c02
Manoel <mancordosa@gmail.com>
2023-04-03 16:10:23 -0300

    Possivel struct a ser usada na arvore

--- src/tree.h
+++ src/tree.h
@@ -1,6 +1,36 @@
 #ifndef TREE_H_
 #define TREE_H_
 
+#include <stdint.h>
+/* #include <stdarg.h> */
+/* typedef struct va_list ; */
+
+enum val_valido { NUM_INT, NUM_DOUBLE, OPERACAO };
+
+typedef struct valor {
+  union {
+    int64_t int_val;
+    double d_val;
+    char op_val;
+  };
+  enum val_valido valido;
+} valor_t;
+
+typedef struct node {
+  node *pai;
+  node *filhos[2];
+  valor_t valor;
+  /* int chave; */
+} node_t;
+
+typedef struct tree {
+  node_t *raiz;
+} tree_t;
+
+void create();
+
 void insert();
+
+void find();
 
 #endif // TREE_H_
