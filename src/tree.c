2129d98066358aa6ca80f66e9234ae377f4cbddd
Manoel <mancordosa@gmail.com>
2023-04-10 11:53:48 -0300

    Initial stuff, will probably be changed later

--- src/tree.c
+++ src/tree.c
@@ -1,1 +1,18 @@
 #include "tree.h"
+#include <stdlib.h>
+
+tree_t create() {
+  tree_t tree;
+  tree.raiz = new_node();
+
+  return tree;
+}
+
+static node_t *new_node() {
+  node_t *node = calloc(1, sizeof(node_t));
+  if (node != NULL) {
+    node->filhos[0] = NULL;
+    node->filhos[1] = NULL;
+  }
+  return node;
+}
